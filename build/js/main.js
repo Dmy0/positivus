c86cb1e753a54d38923042e71beff017af8211db
Dmy0 <popovdmytro00@gmail.com>
2025-01-02 20:16:51 +0200

    positivius

---
+++ build/js/main.js
@@ -0,0 +1,14 @@
+const accordionItems = document.querySelectorAll(".accordion__item");
+
+accordionItems.forEach((item, i) => { 
+    item.addEventListener("click", () => {
+        accordionItems.forEach((item, j) => { 
+            if (i === j) {
+                item.classList.add("_active");
+            } else {
+                item.classList.remove("_active");
+            }
+        });
+    });
+});
+
