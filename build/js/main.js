729862a72e429870b6a16448aef8ef10c1683d39
Dmy0 <popovdmytro00@gmail.com>
2025-01-03 16:18:08 +0200

    positivius

--- build/js/main.js
+++ build/js/main.js
@@ -4,7 +4,7 @@ accordionItems.forEach((item, i) => {
     item.addEventListener("click", (e) => {
         accordionItems.forEach((item, j) => { 
             if (i === j) {
-                item.classList.add("_active");
+               item.classList.contains("_active") ? item.classList.remove("_active") : item.classList.add("_active");
             } else {
                 item.classList.remove("_active");
             }
