e6b70f21ca6276201378b367814a9cc591f123f8
Dmy0 <popovdmytro00@gmail.com>
2025-01-09 18:51:46 +0200

    positivius

--- build/js/main.js
+++ build/js/main.js
@@ -12,5 +12,57 @@ accordionItems.forEach((item, i) => {
     });
 });
 
+const sliderWrap = document.querySelector(".slider")
+      
 
+function setSlider(sliderWrap, slidePosition) {
+    let currentSlide = slidePosition
+    const slides = sliderWrap.querySelectorAll(".slide"),
+          moveLeft = sliderWrap.querySelector(".moveLeft"),
+          moveRight = sliderWrap.querySelector(".moveRight")
+    
+    const addSlidesPosition = () => {
+        slides.forEach((slide, i) =>{
+            if (i === slidePosition) {
+                slide.classList.add("_active") 
+                slides[currentSlide + 1] ? slides[currentSlide + 1].classList.add("_right") : slides[0].classList.add("_right")
+                slides[currentSlide - 1] ? slides[currentSlide - 1].classList.add("_left") : slides[slides.length -1].classList.add("_left")
+            }
+    
+        })  
+    }
+
+    const removeSlidesPosition = () =>{
+        slides.forEach((slide, i) =>{
+            slide.classList.remove("_active")
+            slide.classList.remove("_left")
+            slide.classList.remove("_right")
+        })
+    }
+
+    addSlidesPosition()
+
+    moveLeft.addEventListener("click", () =>{
+        --currentSlide  < 0 ? currentSlide = slides.length -1 : currentSlide
+        removeSlidesPosition()
+        addSlidesPosition()
+        moveLeft.style.pointerEvents = "none"
+        setTimeout(() =>{
+            moveLeft.style.pointerEvents = "initial"
+        }, 700)
+    })
+    
+    moveRight.addEventListener("click", () =>{
+        ++currentSlide > slides.length - 1 ? currentSlide = 0 : currentSlide
+        removeSlidesPosition()
+        addSlidesPosition()
+        moveRight.style.pointerEvents = "none"
+        setTimeout(() =>{
+            moveRight.style.pointerEvents = "initial"
+        }, 700)
+    })
+
+}
+
+setSlider(sliderWrap, 0)
 
